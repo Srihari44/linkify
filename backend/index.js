8e394d9c72c6394fb6cc92c2e0af9e8a2fe51cca
invisiblgeek1 <appumoorthy15@gmail.com>
2021-11-22 16:39:53 +0530

    Authentication middleware and login handler have implemented

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,17 @@
 require("dotenv").config();
 const express = require("express");
-<<<<<<< HEAD
-const app = express();
-const { PORT } = process.env;
-const { createUserData,findUserByEmail } = require("./utils/dbHelper");
-=======
-const { createUserData } = require("./utils/dbHelper");
->>>>>>> 261bb6fe8c292827b200f82ce7c5cc20ca781787
-const { generateAccessToken } = require("./utils/jwtHelper");
-const {verifyToken} = require("./utils/auth")
 
 const app = express();
 const { PORT } = process.env;
+const { createUserData, findUserByEmail } = require("./utils/dbHelper");
+const { generateAccessToken } = require("./utils/jwtHelper");
+const { verifyToken } = require("./utils/auth")
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.post("/register", (req, res) => {
   const { name, email, password } = req.body;
-  if (!(name,email && password)) {
+  if (!(name, email && password)) {
     res.status(400).send("All input is required");
   }
   createUserData({
@@ -34,15 +28,15 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/app",verifyToken,(req,res)=>{
+app.post("/app", verifyToken, (req, res) => {
 
-  res.send({message:"hello world 🎉😂",user:req.user})
-  
+  res.send({ message: "hello world 🎉😂", user: req.user })
+
 })
 
-app.post("/login",async(req,res)=>{
+app.post("/login", async (req, res) => {
 
-  const {email,password}=req.body;
+  const { email, password } = req.body;
 
   if (!(email && password)) {
     res.status(400).send("All input is required");
@@ -50,21 +44,21 @@ app.post("/login",async(req,res)=>{
   const user = await findUserByEmail(email);
 
   try {
-    if (user && password==user.password) {
+    if (user && password == user.password) {
       // Create token
       const token = generateAccessToken({ id: user._id });
       return res.status(200).send({ token });
-  
+
     }
     res.status(400).send("Invalid Credentials");
-    
+
   } catch (error) {
 
     return res.status(code).send({ error });
-    
+
   }
 
-  
+
 
 
 })
